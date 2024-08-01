3205e2cb10afffd4a9707965bdc7e923c5396002
TOUCH Yong An <yongan.touch@kmaker.io>
2024-08-01 22:24:41 +0700

    init project

---
+++ src/test/java/io/kmaker/__min_sharing/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kmaker.__min_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
